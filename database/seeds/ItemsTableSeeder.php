c15ed78538ca5451ab08674e7d71ec21cfed426a
adamzul <adamzulkarnain98@gmail.com>
2019-08-27 15:48:12 +0700

    init

---
+++ database/seeds/ItemsTableSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Item;
+
+class ItemsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Item::truncate();
+
+        $faker = \Faker\Factory::create();
+
+        // And now, let's create a few articles in our database:
+        for ($i = 0; $i < 50; $i++) {
+            Item::create([
+                'name' => $faker->name,
+                'price' => $faker->randomNumber(2)*1000,
+            ]);
+        }
+    }
+}
