e78ed46fe02918d3ae831d0a3c168a514dbfdf70
adamzul <adamzulkarnain98@gmail.com>
2019-08-28 15:54:51 +0700

    implementasi oauth passport

--- routes/api.php
+++ routes/api.php
@@ -12,13 +12,31 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::post('login', 'Api\AuthController@login');
+Route::post('register', 'Api\AuthController@register');
+// Route::prefix('v1')->group(function(){
+    
+//     Route::group(['middleware' => 'auth:api'], function(){
+//     Route::post('getUser', 'Api\AuthController@getUser');
+//     });
+//    });
 
-// Route::middleware('auth:api')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
+Route::get('/not_authenticated', function(Request $request){
+    return response()->json(['message' => 'not authenticated'], 401);
+    // return "tes";
+})->name('login');
 
-Route::get('items', 'ItemController@index');
-Route::get('items/{id}', 'ItemController@show');
-Route::post('items', 'ItemController@store');
-Route::put('items/{id}', 'ItemController@update');
-Route::delete('items/{id}', 'ItemController@delete');
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::group(['middleware' => 'auth:api'], function(){
+    Route::post('details', 'API\UserController@details');
+    Route::get('items', 'ItemController@index');
+    Route::get('items/{id}', 'ItemController@show');
+    Route::post('items', 'ItemController@store');
+    Route::put('items/{id}', 'ItemController@update');
+    Route::delete('items/{id}', 'ItemController@delete');
+});
+
+
